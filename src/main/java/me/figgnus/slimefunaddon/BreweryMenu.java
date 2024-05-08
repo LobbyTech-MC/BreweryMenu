0ce5b214d35635edb399a815c48dc265a8f32e5f
Figgnus <dav33mail@gmail.com>
2024-05-08 21:44:25 +0200

    Update

--- src/main/java/me/figgnus/slimefunaddon/BreweryMenu.java
+++ src/main/java/me/figgnus/slimefunaddon/BreweryMenu.java
@@ -1,5 +1,6 @@
 package me.figgnus.slimefunaddon;
 
+
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -32,13 +33,19 @@ public class BreweryMenu extends JavaPlugin implements SlimefunAddon {
             // You could start an Auto-Updater for example
         }
         // item that represents group
-        ItemStack breweryGroupItem = new CustomItemStack(Material.CAULDRON, "&eBrewery", "", "&aOběvte nádherný svět alkoholu...");
+        ItemStack breweryGroupItem = new CustomItemStack(Material.POTION, "&eBrewery", "", "&aObjevte nádherný svět alkoholu...");
+        ItemMeta meta = breweryGroupItem.getItemMeta();
+        assert meta != null;
+        meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
+        breweryGroupItem.setItemMeta(meta);
+
         // Give your Category a unique id.
         NamespacedKey breweryGroupId = new NamespacedKey(this, "brewery_category");
         // creates group
         ItemGroup breweryGroup = new ItemGroup(breweryGroupId, breweryGroupItem);
 
         //item stacks (needed for creating item)
+        SlimefunItemStack cauldron_stack = new SlimefunItemStack("BREWERY", Material.CAULDRON, "&eBrewery");
         SlimefunItemStack skotska_whisky_stack = new SlimefunItemStack("SKOTSKA_WHISKY", Material.POTION, "&eSkotská Whisky");
         configureMeta(skotska_whisky_stack, createNewColor(186,125,84));
         SlimefunItemStack irska_whisky_stack = new SlimefunItemStack("IRSKA_WHISKY", Material.POTION, "&eIrská Whisky");
@@ -84,23 +91,23 @@ public class BreweryMenu extends JavaPlugin implements SlimefunAddon {
         SlimefunItemStack vajecnak_stack = new SlimefunItemStack("VAJECNAK", Material.POTION, "&eVaječňák");
         configureMeta(vajecnak_stack, createNewColor(255,230,128));
 
-
         //recipe for items
         ItemStack[] item = {null, null, null, null, null, null, null, null, null};
 
-        ItemStack[] skotska_whisky_recipe = {createRecipeItem("Wheat", 9, Material.WHEAT), createRecipeItem("Yeast", 2, Material.BUNDLE), null, null, null, null, createBarrel("Oak", 10), createClock(4), createBrewingStand(2)};
-        ItemStack[] irska_whisky_recipe = {createRecipeItem("Wheat", 8, Material.WHEAT), createRecipeItem("Yeast", 2, Material.BUNDLE), null, null, null, null, createBarrel("Oak", 3), createClock(4), createBrewingStand(3)};
-        ItemStack[] bourbon_recipe = {createRecipeItem("Wheat", 4, Material.WHEAT), createRecipeItem("Corn", 5, Material.SUGAR_CANE), createRecipeItem("Yeast", 1, Material.BUNDLE), null, null, null, createBarrel("Oak", 3), createClock(4), createBrewingStand(2)};
-        ItemStack[] psenicne_pivo_recipe = {createRecipeItem("Wheat", 3, Material.WHEAT), createRecipeItem("Yeast", 1, Material.BUNDLE), null, null, null, null, createBarrel("Birch", 2), createClock(8), null};
-        ItemStack[] pivo_recipe = {createRecipeItem("Wheat", 6, Material.WHEAT), createRecipeItem("Yeast", 2, Material.BUNDLE), null, null, null, null, createBarrel("Jakýkoliv Sud", 3), createClock(8), null};
-        ItemStack[] tmavy_lezak_recipe = {createRecipeItem("Wheat", 12, Material.WHEAT), createRecipeItem("Yeast", 1, Material.BUNDLE), null, null, null, null, createBarrel("Dark Oak", 8), createClock(8), null};
+        ItemStack[] cauldron_recipe = {null, null, null, null, createMultiblockRecipe(Material.CAULDRON, "Naplnit vodou."), null, createMultiblockRecipe(Material.COBBLESTONE_STAIRS, "Oheň musí být obestavený schody."), createMultiblockRecipe(Material.FLINT_AND_STEEL, "Oheň nebo jiný zdroj tepla."), createMultiblockRecipe(Material.COBBLESTONE_STAIRS, "Oheň musí být obestavený schody.")};
+        ItemStack[] skotska_whisky_recipe = {createRecipeItem("Wheat", 9, Material.WHEAT), createRecipeItem("YEAST", "606be2df2122344bda479feece365ee0e9d5da276afa0e8ce8d848f373dd131", "&eYeast", 2), null, null, null, null, createBarrel("Oak", 10), createClock(4), createBrewingStand(2)};
+        ItemStack[] irska_whisky_recipe = {createRecipeItem("Wheat", 8, Material.WHEAT), createRecipeItem("YEAST", "606be2df2122344bda479feece365ee0e9d5da276afa0e8ce8d848f373dd131", "&eYeast", 2), null, null, null, null, createBarrel("Oak", 3), createClock(4), createBrewingStand(3)};
+        ItemStack[] bourbon_recipe = {createRecipeItem("Wheat", 4, Material.WHEAT), createRecipeItem("CORN", "9bd3802e5fac03afab742b0f3cca41bcd4723bee911d23be29cffd5b965f1", "&eCorn", 5), createRecipeItem("YEAST", "606be2df2122344bda479feece365ee0e9d5da276afa0e8ce8d848f373dd131", "&eYeast", 1), null, null, null, createBarrel("Oak", 3), createClock(4), createBrewingStand(2)};
+        ItemStack[] psenicne_pivo_recipe = {createRecipeItem("Wheat", 3, Material.WHEAT), createRecipeItem("YEAST", "606be2df2122344bda479feece365ee0e9d5da276afa0e8ce8d848f373dd131", "&eYeast", 1), null, null, null, null, createBarrel("Birch", 2), createClock(8), null};
+        ItemStack[] pivo_recipe = {createRecipeItem("Wheat", 6, Material.WHEAT), createRecipeItem("YEAST", "606be2df2122344bda479feece365ee0e9d5da276afa0e8ce8d848f373dd131", "&eYeast", 2), null, null, null, null, createBarrel("Jakýkoliv Sud", 3), createClock(8), null};
+        ItemStack[] tmavy_lezak_recipe = {createRecipeItem("Wheat", 12, Material.WHEAT), createRecipeItem("YEAST", "606be2df2122344bda479feece365ee0e9d5da276afa0e8ce8d848f373dd131", "&eYeast", 1), null, null, null, null, createBarrel("Dark Oak", 8), createClock(8), null};
         ItemStack[] cervene_vino_recipe = {createRecipeItem("Sweet Berries", 9, Material.SWEET_BERRIES), null, null, null, null, null, createBarrel("Jakýkoliv Sud", 20), createClock(5), null};
         ItemStack[] bile_vino_recipe = {createRecipeItem("Glow Berries", 9, Material.GLOW_BERRIES), null, null, null, null, null, createBarrel("Jakýkoliv Sud", 20), createClock(5), null};
-        ItemStack[] medovina_recipe = {createRecipeItem("Honey Bottle", 4, Material.HONEY_BOTTLE), createRecipeItem("Sugar", 2, Material.SUGAR), createRecipeItem("Yeast", 2, Material.BUNDLE), null, null, null, createBarrel("Oak", 4), createClock(3), null};
-        ItemStack[] jablecna_medovina_recipe = {createRecipeItem("Honey Bottle", 6, Material.HONEY_BOTTLE), createRecipeItem("Apple", 2, Material.APPLE), createRecipeItem("Yeast", 1, Material.BUNDLE), null, null, null, createBarrel("Oak", 4), createClock(4), null};
+        ItemStack[] medovina_recipe = {createRecipeItem("Honey Bottle", 4, Material.HONEY_BOTTLE), createRecipeItem("Sugar", 2, Material.SUGAR), createRecipeItem("YEAST", "606be2df2122344bda479feece365ee0e9d5da276afa0e8ce8d848f373dd131", "&eYeast", 2), null, null, null, createBarrel("Oak", 4), createClock(3), null};
+        ItemStack[] jablecna_medovina_recipe = {createRecipeItem("Honey Bottle", 6, Material.HONEY_BOTTLE), createRecipeItem("Apple", 2, Material.APPLE), createRecipeItem("YEAST", "606be2df2122344bda479feece365ee0e9d5da276afa0e8ce8d848f373dd131", "&eYeast", 1), null, null, null, createBarrel("Oak", 4), createClock(4), null};
         ItemStack[] jablecny_most_recipe = {createRecipeItem("Apple", 14, Material.APPLE), null, null, null, null, null, createBarrel("Jakýkoliv Sud", 3), createClock(7), null};
         ItemStack[] jablecny_liker_recipe = {createRecipeItem("Apple", 12, Material.APPLE), createRecipeItem("Sugar", 3, Material.SUGAR), null, null, null, null, createBarrel("Accacia", 6), createClock(16), createBrewingStand(3)};
-        ItemStack[] rum_recipe = {createRecipeItem("Corn", 18, Material.SUGAR_CANE), null, null, null, null, null, createBarrel("Oak", 14), createClock(6), createBrewingStand(2)};
+        ItemStack[] rum_recipe = {createRecipeItem("CORN", "9bd3802e5fac03afab742b0f3cca41bcd4723bee911d23be29cffd5b965f1", "&eCorn", 18), null, null, null, null, null, createBarrel("Oak", 14), createClock(6), createBrewingStand(2)};
         ItemStack[] vodka_recipe = {createRecipeItem("Potato", 16, Material.POTATO), null, null, null, null, null, null, createClock(15), createBrewingStand(3)};
         ItemStack[] houbova_vodka_recipe = {createRecipeItem("Potato", 14, Material.POTATO), createRecipeItem("Red Mushroom", 3, Material.RED_MUSHROOM), createRecipeItem("Brown Mushroom", 3, Material.BROWN_MUSHROOM), null, null, null, null, createClock(18), createBrewingStand(5)};
         ItemStack[] gin_recipe = {createRecipeItem("Wheat", 9, Material.WHEAT), createRecipeItem("Blue Orchids", 6, Material.BLUE_ORCHID), createRecipeItem("Apple",1, Material.APPLE), null, null, null, null, createClock(6), createBrewingStand(2)};
@@ -112,33 +119,35 @@ public class BreweryMenu extends JavaPlugin implements SlimefunAddon {
         ItemStack[] vajecnak_recipe = {createRecipeItem("Egg", 5, Material.EGG), createRecipeItem("Sugar", 2, Material.SUGAR), createRecipeItem("Milk Bucket", 1, Material.MILK_BUCKET), null, null, null, createBarrel("Jakýkoliv Sud",3), createClock(2), null};
 
         //recipe type - adds icon of cauldron
-        RecipeType CAULDRON = new RecipeType(breweryGroupId, breweryGroupItem);
+        RecipeType MENU_BOTTLE = new RecipeType(breweryGroupId, breweryGroupItem);
 
         //creates items
-        SlimefunItem skotska_whisky = new SlimefunItem(breweryGroup, skotska_whisky_stack, CAULDRON, skotska_whisky_recipe);
-        SlimefunItem irska_whisky = new SlimefunItem(breweryGroup, irska_whisky_stack, CAULDRON, irska_whisky_recipe);
-        SlimefunItem bourbon = new SlimefunItem(breweryGroup, bourbon_stack, CAULDRON, bourbon_recipe);
-        SlimefunItem psenicne_pivo = new SlimefunItem(breweryGroup, psenicne_pivo_stack, CAULDRON, psenicne_pivo_recipe);
-        SlimefunItem pivo = new SlimefunItem(breweryGroup, pivo_stack, CAULDRON, pivo_recipe);
-        SlimefunItem tmavy_lezak = new SlimefunItem(breweryGroup, tmavy_lezak_stack, CAULDRON, tmavy_lezak_recipe);
-        SlimefunItem cervene_vino = new SlimefunItem(breweryGroup, cervene_vino_stack, CAULDRON, cervene_vino_recipe);
-        SlimefunItem bile_vino = new SlimefunItem(breweryGroup, bile_vino_stack, CAULDRON, bile_vino_recipe);
-        SlimefunItem medovina = new SlimefunItem(breweryGroup, medovina_stack, CAULDRON, medovina_recipe);
-        SlimefunItem jablecna_medovina = new SlimefunItem(breweryGroup, jablecna_medovina_stack, CAULDRON, jablecna_medovina_recipe);
-        SlimefunItem jablecny_most = new SlimefunItem(breweryGroup, jablecny_most_stack, CAULDRON, jablecny_most_recipe);
-        SlimefunItem jablecny_liker = new SlimefunItem(breweryGroup, jablecny_liker_stack, CAULDRON, jablecny_liker_recipe);
-        SlimefunItem rum = new SlimefunItem(breweryGroup, rum_stack, CAULDRON, rum_recipe);
-        SlimefunItem vodka = new SlimefunItem(breweryGroup, vodka_stack, CAULDRON, vodka_recipe);
-        SlimefunItem houbova_vodka = new SlimefunItem(breweryGroup, houbova_vodka_stack, CAULDRON, houbova_vodka_recipe);
-        SlimefunItem gin = new SlimefunItem(breweryGroup, gin_stack, CAULDRON, gin_recipe);
-        SlimefunItem tequila = new SlimefunItem(breweryGroup, tequila_stack, CAULDRON, tequila_recipe);
-        SlimefunItem absinth = new SlimefunItem(breweryGroup, absinth_stack, CAULDRON, absinth_recipe);
-        SlimefunItem zeleny_absinth = new SlimefunItem(breweryGroup, zeleny_absinth_stack, CAULDRON, zeleny_absinth_recipe);
-        SlimefunItem bramborova_polevka = new SlimefunItem(breweryGroup, bramborova_polevka_stack, CAULDRON, bramborova_polevka_recipe);
-        SlimefunItem kava = new SlimefunItem(breweryGroup, kava_stack, CAULDRON, kava_recipe);
-        SlimefunItem vajecnak = new SlimefunItem(breweryGroup, vajecnak_stack, CAULDRON, vajecnak_recipe);
+        SlimefunItem cauldron = new SlimefunItem(breweryGroup, cauldron_stack, RecipeType.MULTIBLOCK, cauldron_recipe);
+        SlimefunItem skotska_whisky = new SlimefunItem(breweryGroup, skotska_whisky_stack, MENU_BOTTLE, skotska_whisky_recipe);
+        SlimefunItem irska_whisky = new SlimefunItem(breweryGroup, irska_whisky_stack, MENU_BOTTLE, irska_whisky_recipe);
+        SlimefunItem bourbon = new SlimefunItem(breweryGroup, bourbon_stack, MENU_BOTTLE, bourbon_recipe);
+        SlimefunItem psenicne_pivo = new SlimefunItem(breweryGroup, psenicne_pivo_stack, MENU_BOTTLE, psenicne_pivo_recipe);
+        SlimefunItem pivo = new SlimefunItem(breweryGroup, pivo_stack, MENU_BOTTLE, pivo_recipe);
+        SlimefunItem tmavy_lezak = new SlimefunItem(breweryGroup, tmavy_lezak_stack, MENU_BOTTLE, tmavy_lezak_recipe);
+        SlimefunItem cervene_vino = new SlimefunItem(breweryGroup, cervene_vino_stack, MENU_BOTTLE, cervene_vino_recipe);
+        SlimefunItem bile_vino = new SlimefunItem(breweryGroup, bile_vino_stack, MENU_BOTTLE, bile_vino_recipe);
+        SlimefunItem medovina = new SlimefunItem(breweryGroup, medovina_stack, MENU_BOTTLE, medovina_recipe);
+        SlimefunItem jablecna_medovina = new SlimefunItem(breweryGroup, jablecna_medovina_stack, MENU_BOTTLE, jablecna_medovina_recipe);
+        SlimefunItem jablecny_most = new SlimefunItem(breweryGroup, jablecny_most_stack, MENU_BOTTLE, jablecny_most_recipe);
+        SlimefunItem jablecny_liker = new SlimefunItem(breweryGroup, jablecny_liker_stack, MENU_BOTTLE, jablecny_liker_recipe);
+        SlimefunItem rum = new SlimefunItem(breweryGroup, rum_stack, MENU_BOTTLE, rum_recipe);
+        SlimefunItem vodka = new SlimefunItem(breweryGroup, vodka_stack, MENU_BOTTLE, vodka_recipe);
+        SlimefunItem houbova_vodka = new SlimefunItem(breweryGroup, houbova_vodka_stack, MENU_BOTTLE, houbova_vodka_recipe);
+        SlimefunItem gin = new SlimefunItem(breweryGroup, gin_stack, MENU_BOTTLE, gin_recipe);
+        SlimefunItem tequila = new SlimefunItem(breweryGroup, tequila_stack, MENU_BOTTLE, tequila_recipe);
+        SlimefunItem absinth = new SlimefunItem(breweryGroup, absinth_stack, MENU_BOTTLE, absinth_recipe);
+        SlimefunItem zeleny_absinth = new SlimefunItem(breweryGroup, zeleny_absinth_stack, MENU_BOTTLE, zeleny_absinth_recipe);
+        SlimefunItem bramborova_polevka = new SlimefunItem(breweryGroup, bramborova_polevka_stack, MENU_BOTTLE, bramborova_polevka_recipe);
+        SlimefunItem kava = new SlimefunItem(breweryGroup, kava_stack, MENU_BOTTLE, kava_recipe);
+        SlimefunItem vajecnak = new SlimefunItem(breweryGroup, vajecnak_stack, MENU_BOTTLE, vajecnak_recipe);
 
         //register items
+        cauldron.register(this);
         skotska_whisky.register(this);
         irska_whisky.register(this);
         bourbon.register(this);
@@ -163,13 +172,14 @@ public class BreweryMenu extends JavaPlugin implements SlimefunAddon {
         vajecnak.register(this);
 
     }
+
     private Color createNewColor(int a, int b, int c){
-        Color color = Color.fromRGB(a, b, c);
-        return color;
+        return Color.fromRGB(a, b, c);
     }
 
     private void configureMeta(SlimefunItemStack item, Color color) {
         PotionMeta meta = (PotionMeta) item.getItemMeta();
+        assert meta != null;
         meta.setColor(color);
         meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
         item.setItemMeta(meta);
@@ -180,12 +190,19 @@ public class BreweryMenu extends JavaPlugin implements SlimefunAddon {
         item.setAmount(amount);
         return item;
     }
+    private SlimefunItemStack createRecipeItem(String id, String name, String textrue, int amount){
+        SlimefunItemStack item = new SlimefunItemStack(id, name, textrue);
+        item.setAmount(amount);
+        return item;
+    }
+
 
     private ItemStack createClock(int i) {
         List<String > lore = new ArrayList<>();
         lore.add(i + " minut");
-        ItemStack clock = new CustomItemStack(Material.CLOCK, "&eDoba Vaření");
+        ItemStack clock = new CustomItemStack(Material.CLOCK, "&aDoba Vaření");
         ItemMeta meta = clock.getItemMeta();
+        assert meta != null;
         meta.setLore(lore);
         clock.setItemMeta(meta);
         return clock;
@@ -194,8 +211,9 @@ public class BreweryMenu extends JavaPlugin implements SlimefunAddon {
     private ItemStack createBrewingStand(int i) {
         List<String> lore = new ArrayList<>();
         lore.add(i + " Krát: ");
-        ItemStack brewingStand = new CustomItemStack(Material.BREWING_STAND, "&ePočet Destilace");
+        ItemStack brewingStand = new CustomItemStack(Material.BREWING_STAND, "&aPočet Destilace");
         ItemMeta meta = brewingStand.getItemMeta();
+        assert meta != null;
         meta.setLore(lore);
         brewingStand.setItemMeta(meta);
         return brewingStand;
@@ -205,11 +223,22 @@ public class BreweryMenu extends JavaPlugin implements SlimefunAddon {
         List<String> lore = new ArrayList<>();
         lore.add("Sud: " + s);
         lore.add("Čas: " + i + " let");
-        ItemStack barrel = new CustomItemStack(Material.BARREL, "&eTyp Sudu");
+        ItemStack barrel = new CustomItemStack(Material.BARREL, "&aTyp Sudu");
         ItemMeta meta = barrel.getItemMeta();
+        assert meta != null;
         meta.setLore(lore);
         barrel.setItemMeta(meta);
         return barrel;
+    }
+    private ItemStack createMultiblockRecipe(Material material, String lore_input){
+        ItemStack item = new CustomItemStack(material);
+        ItemMeta meta = item.getItemMeta();
+        List<String> lore = new ArrayList<>();
+        lore.add(lore_input);
+        assert meta != null;
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+        return item;
     }
 
     @Override
@@ -230,5 +259,7 @@ public class BreweryMenu extends JavaPlugin implements SlimefunAddon {
     //        swMeta.setColor(Color.LIME);
     //        swMeta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
     //        skotska_whisky_stack.setItemMeta(swMeta);
+
+    // corn "9bd3802e5fac03afab742b0f3cca41bcd4723bee911d23be29cffd5b965f1"
 
 }
